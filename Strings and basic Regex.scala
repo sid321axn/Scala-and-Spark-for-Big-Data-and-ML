98adb05779ab98135b01286eda202808cd9ab29f
Manu Siddhartha <sid321axn@gmail.com>
2019-06-26 18:35:59 +0530

    Add files via upload

---
+++ Strings and basic Regex.scala
@@ -0,0 +1,131 @@
+// Databricks notebook source
+println("hello")
+
+// COMMAND ----------
+
+println('hello')
+
+// COMMAND ----------
+
+val farewell = "good" + "bye"
+
+// COMMAND ----------
+
+"good"+"afternoon"
+
+// COMMAND ----------
+
+"dance"*5
+
+// COMMAND ----------
+
+val st ="hello"
+
+// COMMAND ----------
+
+st
+
+// COMMAND ----------
+
+st.length
+
+// COMMAND ----------
+
+st.length()
+
+// COMMAND ----------
+
+val name ="Manu Siddhartha"
+
+// COMMAND ----------
+
+val greet = s"Hello ${name}"
+
+// COMMAND ----------
+
+val greet = s"hello $name"
+
+// COMMAND ----------
+
+val greet = f"hello $name"
+
+// COMMAND ----------
+
+printf("A  string %s, an integer %d, a float %f","Hi",10,12.67)
+
+// COMMAND ----------
+
+printf("A float %1.2f",1.2345)
+
+// COMMAND ----------
+
+printf("A float %1.2f",1.2395)
+
+// COMMAND ----------
+
+val st="A long basic Journey"
+
+// COMMAND ----------
+
+st.charAt(0)
+
+// COMMAND ----------
+
+st.charAt(3)
+
+// COMMAND ----------
+
+st.indexOf("a")
+
+// COMMAND ----------
+
+st slice (0,4)
+
+// COMMAND ----------
+
+st slice (5,6)
+
+// COMMAND ----------
+
+val st ="term1"
+
+// COMMAND ----------
+
+st matches "term1"
+
+// COMMAND ----------
+
+st matches "term2"
+
+// COMMAND ----------
+
+st contains "term1"
+
+// COMMAND ----------
+
+st contains "term2"
+
+// COMMAND ----------
+
+// DBTITLE 1,Tuple
+(1,2,2.2,"hello")
+
+// COMMAND ----------
+
+(3,1,(2,3))
+
+// COMMAND ----------
+
+res18
+
+// COMMAND ----------
+
+res18._3
+
+// COMMAND ----------
+
+res18._4
+
+// COMMAND ----------
+
+
