cad2c5cca37e82e85dfd682cb8473b1b655bfe0e
Manu Siddhartha <sid321axn@gmail.com>
2019-06-26 17:55:51 +0530

    Add files via upload

---
+++ Values and Variables.scala
@@ -0,0 +1,42 @@
+// Databricks notebook source
+var myvar =100
+
+// COMMAND ----------
+
+myvar=5000
+
+// COMMAND ----------
+
+val pos =1000
+
+// COMMAND ----------
+
+myvar * pos
+
+// COMMAND ----------
+
+pos = "hello"
+
+// COMMAND ----------
+
+myvar = "helloo"
+
+// COMMAND ----------
+
+val c =12
+
+// COMMAND ----------
+
+val my_string = "My string hello"
+
+// COMMAND ----------
+
+val 2my_string="Hello"
+
+// COMMAND ----------
+
+val `my.string`="hi"
+
+// COMMAND ----------
+
+
---
+++ scala_tutorial.scala
@@ -0,0 +1,59 @@
+// Databricks notebook source
+// DBTITLE 1,Arithematic & Numbers
+100
+
+// COMMAND ----------
+
+2.5
+
+// COMMAND ----------
+
+1 + 1
+
+// COMMAND ----------
+
+2 - 1
+
+// COMMAND ----------
+
+2*5
+
+// COMMAND ----------
+
+1/2
+
+// COMMAND ----------
+
+1.0/2
+
+// COMMAND ----------
+
+1 / 2.0
+
+// COMMAND ----------
+
+1.0/2.0
+
+// COMMAND ----------
+
+math.pow(2,4)
+
+// COMMAND ----------
+
+res0
+
+// COMMAND ----------
+
+res0 + res5
+
+// COMMAND ----------
+
+1 + 2*3 + 4
+
+// COMMAND ----------
+
+(1+2)*(3+5)
+
+// COMMAND ----------
+
+
